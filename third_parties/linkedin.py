d11c297c783d414a321d9422302c7ab23bc38f04
AmirSTM <amirstm@gmail.com>
2024-12-23 13:07:59 +0330

    Complete linkedin section

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -22,6 +22,15 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
             timeout=10,
         )
     data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
     return data
 
 
