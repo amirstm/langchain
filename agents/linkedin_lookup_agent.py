23159ff07154f5f2b5aa2c22b1230cbbdd81f2a8
AmirSTM <amirstm@gmail.com>
2024-12-29 13:29:46 +0330

    Implement lookup agent

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -10,4 +10,35 @@ load_dotenv()
 
 
 def lookup(name: str) -> str:
-    return None
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    template = """\
+given the full name {name_of_person} I want you to get it me a link to their LinkedIn profile page.\
+Your answer should contain only a URL.\
+"""
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google for LinkedIn profile page",
+            func="?",
+            description="Useful for when you need to get the LinkedIn page URL",
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linkedin_profile_url = result["output"]
+    return linkedin_profile_url
+
+
+if __name__ == "__main__":
+    linkedin_url = lookup(name="Amir Zare")
+    print(linkedin_url)
