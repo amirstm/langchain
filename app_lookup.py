844719cad5b5eb090b73484a1425be9698cf119a
AmirSTM <amirstm@gmail.com>
2025-01-05 15:46:56 +0330

    Init flask application

--- app_lookup.py
+++ app_lookup.py
@@ -1,12 +1,13 @@
+from typing import Tuple
 from dotenv import load_dotenv
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 
 
-def custom_lookup(name: str) -> str:
+def custom_lookup(name: str) -> Tuple[Summary, str]:
     linkedin_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(
         linkedin_profile_url=linkedin_url, mock=True
@@ -29,9 +30,9 @@ Given the information {information} about a person from I want you to create:
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={"information": linkedin_data})
+    res: Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data["profile_pic_url"]
 
 
 if __name__ == "__main__":
