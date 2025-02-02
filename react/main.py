6c35e9fb4d352d61fb6aa236db713a278f09ac50
AmirSTM <amirstm@gmail.com>
2025-02-02 14:13:11 +0330

    Convert get_text_length to langchain tool

--- react/main.py
+++ react/main.py
@@ -1,13 +1,18 @@
 from dotenv import load_dotenv
+from langchain.agents import tool
 
 load_dotenv()
 
 
+@tool
 def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip('"')
     return len(text)
 
 
 if __name__ == "__main__":
     print("Hello ReAct Langchain!")
-    print(get_text_length(text="Hello"))
+    print(get_text_length.description)
+    print(get_text_length.invoke(input={"text": "Sample"}))
