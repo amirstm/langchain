fdfa4f7076370ee667c4399231056806982494fa
AmirSTM <amirstm@gmail.com>
2025-02-05 15:13:51 +0330

    Initiate llm in react section

--- react/main.py
+++ react/main.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 from langchain.agents import tool
+from langchain_core.prompts import PromptTemplate
+from langchain.tools.render import render_text_description
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
@@ -37,6 +40,12 @@ if __name__ == "__main__":
     Begin!
 
     Question: {input}
-    Thought:{agent_scratchpad}
+    Thought:
     """
-    print(template)
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools),
+        tool_names=", ".join([t.name for t in tools]),
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
