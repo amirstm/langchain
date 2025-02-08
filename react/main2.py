6343fbc079f59ee1d46bdc82753904380c98d10b
AmirSTM <amirstm@gmail.com>
2025-02-08 13:28:05 +0330

    Add agent step

--- react/main2.py
+++ react/main2.py
@@ -1,13 +1,13 @@
-from dotenv import load_dotenv
-from langchain.agents import tool, Tool
-from langchain.agents.output_parsers import (
-    ReActSingleInputOutputParser,
-)
-from langchain_core.prompts import PromptTemplate
-from langchain.tools.render import render_text_description
-from langchain_openai import ChatOpenAI
-from langchain.schema import AgentAction, AgentFinish
 from typing import Union, List
+
+from dotenv import load_dotenv
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import AgentAction, AgentFinish
+from langchain.tools import Tool, tool
+from langchain.tools.render import render_text_description
+
 
 load_dotenv()
 
@@ -16,7 +16,10 @@ load_dotenv()
 def get_text_length(text: str) -> int:
     """Returns the length of a text by characters"""
     print(f"get_text_length enter with {text=}")
-    text = text.strip("'\n").strip('"')
+    text = text.strip("'\n").strip(
+        '"'
+    )  # stripping away non alphabetic characters just in case
+
     return len(text)
 
 
@@ -24,22 +27,20 @@ def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
         if tool.name == tool_name:
             return tool
-    raise ValueError(f"Tool with name {tool_name} not found")
+    raise ValueError(f"Tool wtih name {tool_name} not found")
 
 
 if __name__ == "__main__":
-    print("Hello ReAct Langchain!")
-    # print(get_text_length.description)
-    # print(get_text_length.invoke(input={"text": "Sample"}))
-
+    print("Hello ReAct LangChain!")
     tools = [get_text_length]
+
     template = """
     Answer the following questions as best you can. You have access to the following tools:
 
     {tools}
-
+    
     Use the following format:
-
+    
     Question: the input question you must answer
     Thought: you should always think about what to do
     Action: the action to take, should be one of [{tool_names}]
@@ -48,9 +49,9 @@ if __name__ == "__main__":
     ... (this Thought/Action/Action Input/Observation can repeat N times)
     Thought: I now know the final answer
     Final Answer: the final answer to the original input question
-
+    
     Begin!
-
+    
     Question: {input}
     Thought:
     """
@@ -61,22 +62,27 @@ if __name__ == "__main__":
     )
 
     llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation"])
+    intermediate_steps = []
     agent = (
-        {"input": lambda x: x["input"]} | prompt | llm | ReActSingleInputOutputParser()
+        {
+            "input": lambda x: x["input"],
+        }
+        | prompt
+        | llm
+        | ReActSingleInputOutputParser()
     )
 
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+    res = agent.invoke(
         {
-            "input": "What is the length of 'Doggie' in characters?",
+            "input": "What is the length of 'DOG' in characters?",
         }
     )
-    print(agent_step)
+    print(res)
 
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
-        tool_to_use = find_tool_by_name(tools, tool_name)
-        tool_input = agent_step.tool_input
+    # if isinstance(agent_step, AgentAction):
+    #     tool_name = agent_step.tool
+    #     tool_to_use = find_tool_by_name(tools, tool_name)
+    #     tool_input = agent_step.tool_input
 
-        observation = tool_to_use.func(str(tool_input))
-
-        print(f"{observation=}")
+    #     observation = tool_to_use.func(str(tool_input))
+    #     print(f"{observation=}")
